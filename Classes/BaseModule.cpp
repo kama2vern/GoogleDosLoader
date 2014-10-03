50a960549eb0ef7642b14e54a83ee81e3350b69a
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2014-10-03 21:11:06 +0900

    Add base module interface

---
+++ Classes/BaseModule.cpp
@@ -0,0 +1,13 @@
+//
+//  BaseModule.cpp
+//  DiverDark
+//
+//  Created by S_Wyvern on 2014/08/23.
+//
+//
+
+#include "BaseModule.h"
+
+BaseModule::~BaseModule()
+{
+}
---
+++ Classes/BaseModule.h
@@ -0,0 +1,18 @@
+//
+//  BaseModule.h
+//  DiverDark
+//
+//  Created by S_Wyvern on 2014/08/23.
+//
+//
+
+#ifndef DiverDark_BaseModule_h
+#define DiverDark_BaseModule_h
+
+class BaseModule
+{
+public:
+    virtual ~BaseModule();
+};
+
+#endif
